3133aa4ea655b1209a6d20e01e909d71cbc6caa6
ben.zhao <ben.zhao@cdskysoft.com>
2019-11-28 11:26:43 +0800

    Test

---
+++ app/src/main/java/xw/com/gittestdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package xw.com.gittestdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
