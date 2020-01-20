b1483f2104eac07b534353e8b837e160e96776a6
ben.zhao <ben.zhao@cdskysoft.com>
2020-01-20 14:03:00 +0800

    Commit onResume

--- app/src/main/java/xw/com/gittestdemo/MainActivity.java
+++ app/src/main/java/xw/com/gittestdemo/MainActivity.java
@@ -8,10 +8,17 @@ public class MainActivity extends AppCompatActivity {
  private int c=0;
  private int b=1;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
     }
 }
