9c3797507d6142fbcdd448c8acbd381258e0cc7f
ben.zhao <ben.zhao@cdskysoft.com>
2020-01-20 15:00:52 +0800

    88888

--- app/src/main/java/xw/com/gittestdemo/MainActivity.java
+++ app/src/main/java/xw/com/gittestdemo/MainActivity.java
@@ -45,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+//        if (){
+//
+//        }
 
     }
+
 }
