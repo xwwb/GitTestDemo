20dc078ad4c75df3169ca464c99e403ac5b9beb6
ben.zhao <ben.zhao@cdskysoft.com>
2020-01-20 13:55:51 +0800

    Commit Pubc-int c

--- app/src/main/java/xw/com/gittestdemo/MainActivity.java
+++ app/src/main/java/xw/com/gittestdemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
  private  int i=0;
+ private int c=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
